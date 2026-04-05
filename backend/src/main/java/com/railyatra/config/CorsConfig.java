845579de3396283a214d5707b4a46a096fe469d5
HarishKumar222 <narapureddygarikumarreddy@gmail.com>
2026-04-05 19:47:22 +0530

    "changes source path"

--- backend/src/main/java/com/railyatra/config/CorsConfig.java
+++ backend/src/main/java/com/railyatra/config/CorsConfig.java
@@ -24,8 +24,7 @@ public class CorsConfig {
         config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", config);
-        source.registerCorsConfiguration("/ws/**", config);
+        source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 }
